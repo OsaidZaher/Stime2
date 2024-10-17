d59628b07d7af5093d75706fb8a2617e92ad54e4
Osaid Zaher <bigmanfamdamn@gmail.com>
2024-10-17 19:30:44 +0100

    fixing vercel deployment try 3

--- app/api/functionality/comparative-graph/route.ts
+++ app/api/functionality/comparative-graph/route.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth";
 import type { Session } from "next-auth";
 import { authOptions } from "@/app/auth.config";
 
--- app/api/functionality/subjectAdd/route.ts
+++ app/api/functionality/subjectAdd/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import { getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/auth.config";
 
 export async function POST(request: Request) {

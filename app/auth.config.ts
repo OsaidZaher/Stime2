86a033790eba7a7773615ec3acacd9d6f0fcee22
Osaid Zaher <bigmanfamdamn@gmail.com>
2024-11-04 00:32:56 +0000

    added auth secret

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -6,6 +6,7 @@ import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       credentials: {

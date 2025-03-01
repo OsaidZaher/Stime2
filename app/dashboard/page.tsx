9ccaa8289fac9da1982f25d53f4a1a04714c0e56
Osaid Zaher <bigmanfamdamn@gmail.com>
2025-03-01 22:04:52 +0000

    fun using useCOntext but realised sidebar inset was a thing

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,18 +3,23 @@ import LogInCard from "@/components/ui/logInCard";
 import SubjectGoalsCard from "@/components/ui/subjectGoals";
 import DashTimeCard from "@/components/ui/dashTimeCard";
 import TodoListCard from "@/components/ui/toDo";
+import { SideBarMotion } from "@/components/ui/sideBarMotion";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset } from "@/components/ui/sidebar";
 
 export default function Page() {
   return (
     <>
-      <div className="flex space-x-10 ">
-        <div className="space-y-16">
-          <LogInCard />
-          <DashTimeCard />
+      <SidebarInset className="p-8 ml-28">
+        <div className="flex space-x-10 w-full">
+          <div className="space-y-16">
+            <LogInCard />
+            <DashTimeCard />
+          </div>
+          {<SubjectGoalsCard />}
+          <TodoListCard />
         </div>
-        {<SubjectGoalsCard />}
-        <TodoListCard />
-      </div>
+      </SidebarInset>
     </>
   );
 }
--- components/ui/sideBarMotion.tsx
+++ components/ui/sideBarMotion.tsx
@@ -1,4 +1,14 @@
 "use client";
 
 import React from "react";
-import { useSidebar } from "@/components/ui/sidebar";
+import { useSidebar } from "./sidebar";
+
+export function SideBarMotion({ children }: { children: React.ReactNode }) {
+  const { open } = useSidebar();
+
+  return (
+    <>
+      <div className={`${open ? "ml-32" : `ml-0`}`}>{children}</div>
+    </>
+  );
+}
